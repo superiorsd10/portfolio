b31d3189df9eee32a0fe60d4fb7365f5655e0b99
superiorsd10 <sachindapkara6@gmail.com>
2024-03-07 18:54:52 +0530

    feat(theme): add the header

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { use } from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import footer from './components/ui/footer'
+import  footer  from './components/ui/footer'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span><strong>superiorsd10</strong></span>,
@@ -10,6 +11,16 @@ const config: DocsThemeConfig = {
   },
   search: {
     placeholder: "Search..."
+  },
+  head: () => {
+    const router = useRouter();
+
+    const currentPageTitle = router.pathname === '/' ? 'superiorsd10' : router.pathname.replace('/', '');
+    return (
+      <>
+        <title>{currentPageTitle}</title>
+      </>
+    );
   },
   footer: {
     text: footer,
