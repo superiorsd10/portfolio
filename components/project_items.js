5b772824ae7d077db8f502a79bbfed4c58c91e8c
superiorsd10 <sachindapkara6@gmail.com>
2024-03-08 02:17:29 +0530

    refactor(images): replace all png, jpeg by webp

--- components/project_items.js
+++ components/project_items.js
@@ -6,7 +6,7 @@ export const projectItems = [
     {
         title: "EduHub-AI",
         description: "Revolutionizing education with AI, enhancing learning seamlessly.",
-        header: <img src="/images/eduhub-ai.png" className="rounded-sm"></img>,
+        header: <img src="/images/eduhub-ai.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconNotebook className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/superiorsd10/EduHub-AI",
@@ -14,7 +14,7 @@ export const projectItems = [
     {
         title: "Ultimate Alarm Clock",
         description: "Revolutionary alarm clock with smart features for ultimate waking.",
-        header: <img src="/images/ultimate-alarm-clock.png" className="rounded-sm"></img>,
+        header: <img src="/images/ultimate-alarm-clock.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconClock className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/CCExtractor/ultimate_alarm_clock/pulls?q=is:pr+is:closed+author:superiorsd10",
@@ -22,7 +22,7 @@ export const projectItems = [
     {
         title: "Confess",
         description: "Confide freely and anonymously, unburden your heart and mind.",
-        header: <img src="/images/confess.png" className="rounded-sm"></img>,
+        header: <img src="/images/confess.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconPencil className="h-4 w-4 text-neutral-500" />,
         link: "https://play.google.com/store/apps/details?id=com.sachindapkara.confess&hl=en_IN&gl=US",
@@ -30,7 +30,7 @@ export const projectItems = [
     {
         title: "Dash AI",
         description: "Open-source Flutter coding assistant for seamless development.",
-        header: <img src="/images/dash-ai.png" className="rounded-sm"></img>,
+        header: <img src="/images/dash-ai.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconRobot className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/Welltested-AI/dash-ai/pulls?q=is:pr+is:closed+author:superiorsd10",
@@ -38,7 +38,7 @@ export const projectItems = [
     {
         title: "Snake Bit",
         description: "Choose from three modes, slither to high scores in Snake Bit.",
-        header: <img src="/images/snake-bit.png" className="rounded-sm"></img>,
+        header: <img src="/images/snake-bit.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconDeviceGamepad className="h-4 w-4 text-neutral-500" />,
         link: "https://play.google.com/store/apps/details?id=com.sachindapkara.snakebit&hl=en_IN&gl=US",
@@ -46,7 +46,7 @@ export const projectItems = [
     {
         title: "Mirai",
         description: "Build stunning cross-platform apps with real-time JSON using Mirai.",
-        header: <img src="/images/mirai.jpeg" className="rounded-sm"></img>,
+        header: <img src="/images/mirai.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconFrame className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/Securrency-OSS/mirai/pulls?q=is:pr+is:closed+author:superiorsd10",
@@ -54,7 +54,7 @@ export const projectItems = [
     {
         title: "College GateKeeper",
         description: "Automate student intime/outtime marking using machine learning.",
-        header: <img src="/images/college-gatekeeper.png" className="rounded-sm"></img>,
+        header: <img src="/images/college-gatekeeper.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconActivity className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/superiorsd10/college-gatekeeper",
@@ -62,7 +62,7 @@ export const projectItems = [
     {
         title: "Block ToDo",
         description: "Complex backend, simple frontend: Todo App powered by Polygon blockchain.",
-        header: <img src="/images/block-todo.jpeg" className="rounded-sm"></img>,
+        header: <img src="/images/block-todo.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconCheckbox className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/superiorsd10/block-todo",
@@ -70,7 +70,7 @@ export const projectItems = [
     {
         title: "Agrico",
         description: "Empowering organic farmers to increase production yield with data insights.",
-        header: <img src="/images/agrico.jpeg" className="rounded-sm"></img>,
+        header: <img src="/images/agrico.webp" className="rounded-sm"></img>,
         className: "md:col-span-1",
         icon: <IconApple className="h-4 w-4 text-neutral-500" />,
         link: "https://github.com/superiorsd10/agrico",
