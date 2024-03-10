292ebbf2b87eeb07e1ae30bcbb2affec224001c4
superiorsd10 <sachindapkara6@gmail.com>
2024-03-11 02:54:06 +0530

    feat(utils): add the blog items list

---
+++ utils/blog_items.ts
@@ -0,0 +1,8 @@
+export const blogItems = [
+    {
+        title: "How I Implemented Chat with PDF Feature using Atlas Vector Search and RAG Architecture?",
+        description: `celery, pub/sub, text-embedding, cosine-similarity, atlas-vector-search, rag, Mar 10 2024`,
+        className: "md:col-span-2",
+        link: "http://localhost:3000/blogs/01-how-i-implemented-atlas-vector-search-and-rag-for-chat-with-pdf"
+    },
+]
