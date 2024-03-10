5f764bbdac29719cdada72a03c16b87a6b0209d8
superiorsd10 <sachindapkara6@gmail.com>
2024-03-11 02:59:10 +0530

    chore(blog-01): changes in description

--- utils/blog_items.ts
+++ utils/blog_items.ts
@@ -1,7 +1,7 @@
 export const blogItems = [
     {
         title: "How I Implemented Chat with PDF Feature using Atlas Vector Search and RAG Architecture?",
-        description: `celery, pub/sub, text-embedding, cosine-similarity, atlas-vector-search, rag, Mar 10 2024`,
+        description: `Sun, Mar 10 2024`,
         className: "md:col-span-2",
         link: "https://superiorsd10.vercel.app/blogs/01-how-i-implemented-atlas-vector-search-and-rag-for-chat-with-pdf"
     },
