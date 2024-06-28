c7b0e2e66725fa0a44b43c0df00eb0076b7f5c51
superiorsd10 <sachindapkara6@gmail.com>
2024-06-28 16:17:31 +0530

    feat(utils): add bits items

---
+++ utils/bits_items.ts
@@ -0,0 +1,15 @@
+interface BitsItem {
+    title: string;
+    link: string;
+}
+
+export const bitsItems: BitsItem[] = [
+    {
+        title: '1. nanoid',
+        link: '/bits/1-nanoid'
+    },
+    {
+        title: '0. pubsub',
+        link: '/bits/0-pubsub'
+    }
+]
